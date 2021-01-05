cdcc3aaac00bde5be48bcdea0fcac12675a46d5e
varvara-krasavina <varvara.krasavina@gmail.com>
2021-01-05 03:37:48 +0300

    Dockerize mlflow + flask

--- build.sh
+++ build.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
 docker-compose --env-file .env build
-docker run -d -p 5000:5000 --name mlflow language_identification_web
+
+docker run -d \
+    -p 5000:5000 \
+    --mount type=bind,source="$(pwd)"/mlruns,target=/opt/mlflow/mlruns \
+    --mount type=bind,source="$(pwd)"/input,target=/opt/mlflow/input \
+    --name mlflow \
+    language_identification_web
+
 docker start mlflow
+
+docker run -d \
+    -p 8080:8080 \
+    --name serve \
+    language_identification_serve
+
+docker start serve
--- src/config.py
+++ src/config.py
@@ -4,5 +4,3 @@ from dotenv import load_dotenv
 load_dotenv()
 
 LOCAL_MODEL_STORAGE = os.environ.get('LOCAL_MODEL_STORAGE', None)
-
-# DEFAULT_MODEL_PATH = 
